5c8251e7ec2b174fae5b82e5010f150f2ac2c808
Egor Druzhinin <egor.druzhinin97@gmail.com>
2018-12-04 18:51:23 +0500

    Bug fixed

--- flac/song.py
+++ flac/song.py
@@ -1,4 +1,5 @@
 from typing import TextIO
+from threading import Thread
 
 from pyaudio import PyAudio, paInt16, paInt32
 from pydub import AudioSegment
@@ -7,20 +8,21 @@ from pydub.utils import make_chunks
 from meta import Metadata
 
 
-class Song:
+class Song(Thread):
     def __init__(self, filename: str):
         self.meta = Metadata(filename)
         self._segment = AudioSegment.from_file(filename)
         self._audio = PyAudio()
-        self._stream = self._open_stream()
         self._is_paused = True
+
+        Thread.__init__(self)
+        self.start()
 
     def _open_stream(self):
         if self.meta.sample_width not in [16, 24]:
             raise ValueError('This frame width are not supported')
 
         format = paInt16 if self.meta.sample_width == 16 else paInt32
-
         return self._audio.open(
             format=format, channels=self.meta.channels,
             rate=self.meta.frame_rate, output=True)
@@ -32,6 +34,7 @@ class Song:
         self._is_paused = False
 
     def run(self):
+        stream = self._open_stream()
         chunk_count = 0
         chunks = make_chunks(self._segment, 100)
         while chunk_count <= len(chunks):
@@ -39,9 +42,9 @@ class Song:
                 data = chunks[chunk_count]._data
                 chunk_count += 1
             else:
-                free = self._stream.get_write_available()
+                free = stream.get_write_available()
                 data = chr(0) * free
-            self._stream.write(data)
+            stream.write(data)
 
-        self._stream.stop_stream()
+        stream.stop_stream()
         self._audio.terminate()
